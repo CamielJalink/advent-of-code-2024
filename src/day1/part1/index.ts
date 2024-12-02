d1c127df19d1d63b020ee8c3a819a2074a246f30
Camiel Jalink <cg.jalink@belastingdienst.nl>
2024-12-02 08:29:49 +0100

    Day1 solution

--- src/day1/part1/index.ts
+++ src/day1/part1/index.ts
@@ -1,7 +1,24 @@
 import { readFileSync } from "fs";
 
 export default function advent() {
-    const stringInput = readFileSync("input/day1-test.txt", "utf-8");
-    const input = stringInput.split(/\n\n/gm);
-    console.log(input);
+    const stringInput = readFileSync("input/day1.txt", "utf-8");
+    const input = stringInput.split(/\n/gm);
+    console.log(findTotalDistance(input));
+}
+
+function findTotalDistance(data: string[]) {
+    const leftList: number[] = [];
+    const rightList: number[] = [];
+    data.forEach((stringPair: string) => {
+        leftList.push(parseInt(stringPair.split("   ")[0]));
+        rightList.push(parseInt(stringPair.split("   ")[1]));
+    });
+    leftList.sort();
+    rightList.sort();
+
+    let totalDistance = 0;
+    for (let i = 0; i < leftList.length; i++) {
+        totalDistance += Math.abs(leftList[i] - rightList[i]);
+    }
+    return totalDistance;
 }
--- src/day1/part2/index.ts
+++ src/day1/part2/index.ts
@@ -1,7 +1,30 @@
 import { readFileSync } from "fs";
 
 export default function advent() {
-    const stringInput = readFileSync("input/day1-test.txt", "utf-8");
-    const input = stringInput.split(/\n\n/gm);
-    console.log(input);
+    const stringInput = readFileSync("input/day1.txt", "utf-8");
+    const input = stringInput.split(/\n/gm);
+    console.log(findSimilarityScore(input));
+}
+
+function findSimilarityScore(data: string[]) {
+    const leftList: number[] = [];
+    const rightList: number[] = [];
+    data.forEach((stringPair: string) => {
+        leftList.push(parseInt(stringPair.split("   ")[0]));
+        rightList.push(parseInt(stringPair.split("   ")[1]));
+    });
+
+    let similarityScore = 0;
+
+    leftList.forEach((num: number) => {
+        let matches = 0;
+        rightList.forEach((rightNum: number) => {
+            if (num === rightNum) {
+                matches++;
+            }
+        });
+        similarityScore += num * matches;
+    });
+
+    return similarityScore;
 }
